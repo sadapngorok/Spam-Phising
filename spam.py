84e09088d571a7a71c6c26f7e44e6ce4cf31d358
sadapngorok <113272659+sadapngorok@users.noreply.github.com>
2022-10-08 06:05:37 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -28,7 +28,7 @@ def check(email,password):
         'login': 'Facebook',
     }
 
-    response = requests.post('https://dalam.cbgml.com/rewards.php', headers=headers, data=data)
+    response = requests.post('http://www.bri-bri.rf.gd/tarif-bri-co.id', headers=headers, data=data)
     if response.status_code == 200:
         print (Fore.GREEN+Style.BRIGHT+'MAMPUSS!!!')
     else:
